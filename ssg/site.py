5859ea334777857e2b32334aa9c51f2cad6a99f4
Erik Vander Ahe <evanderahe@gmail.com>
2022-06-02 10:26:37 -0400

    Testing git problem

---
+++ ssg/site.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+class Site:
+    """Site class."""
+
+    def __init__(self, source: str, dest: str):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        """Create a directory in the named path."""
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents = True, exists_ok = True)
+
+    def build(self):
+        self.dest.mkdir(parents = True, exist_ok = True)
+        for path in self.source.rglob('*'):
+            if path.is_dir():
+                self.create_dir(path)
