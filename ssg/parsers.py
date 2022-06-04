3551d5825f0494b4ebb44f2a131877b62828ae0b
Erik Vander Ahe <evanderahe@gmail.com>
2022-06-04 14:02:30 -0400

    Fix open call in read() method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -12,7 +12,7 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-        with open(path) as file:
+        with open(path, "r") as file:
             return file.read()
 
     def write(self, path: Path, dest: Path, content, ext: str = ".html"):
