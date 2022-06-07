ea2b620a70b94cf9ce7a2e9bb160d5db8d7190c0
Erik Vander Ahe <evanderahe@gmail.com>
2022-06-07 17:20:35 -0400

    Added error to trigger checker to work.

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,7 +4,7 @@ import shutil
 
 class Parser:
     """Parser class."""
-    extensions: List[str] = []
+    extensions: List[str] = ()
 
     def valid_extension(self, extension):
         return extension in self.extensions
